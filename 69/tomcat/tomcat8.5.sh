c5aa6a32dc8acba508c7ca97fca463329daa8f4a
刘成 <790809568@qq.com>
2018-01-21 13:21:15 +0800

    first commit

--- 69/tomcat/tomcat8.5.sh
+++ 69/tomcat/tomcat8.5.sh
@@ -6,8 +6,8 @@ DOCKER_NAME="tomcat-8.5"
 WAR_PATH="/root/source/26"
 function start_app() {
     compile_app
-    docker cp ${WAR_PATH}/*.war ${DOCKER_NAME}:/usr/local/tomcat/webapps &
-    docker restart ${DOCKER_NAME}
+    docker cp ${WAR_PATH}/*.war tomcat-8.5:/usr/local/tomcat/webapps &
+    docker restart tomcat-8.5
 }
 function compile_app(){
    ${SCRIPT_URL}/git-taoke.sh ant
